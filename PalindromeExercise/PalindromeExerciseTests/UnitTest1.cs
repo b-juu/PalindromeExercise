45dbbb9bc57f0f56c7770365cf52af79520e8c9e
b-juu <erichassett4@gmail.com>
2023-10-23 12:58:31 -0400

    more tests added

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -7,7 +7,11 @@ namespace PalindromeExerciseTests
     public class UnitTest1
     {
         [Theory]
-        [InlineData("racecar", true)] 
+        [InlineData("racecar", true)]
+        [InlineData("signa te signa temere me tangis et angis", true)]
+        [InlineData("roma tibi subito motibus ibit amor", true)]
+        [InlineData("sator arepo tenet opera rotas", true)]
+        [InlineData("in girum imus nocte et consumimur igni", true)]
         public void IsAPalindromeTest(string input, bool expected)
         {          
             WordSmith wordSmith = new WordSmith();
