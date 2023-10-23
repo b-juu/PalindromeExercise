294f0109dae5770054ceb468f37ecec3d154f3ab
b-juu <erichassett4@gmail.com>
2023-10-23 11:17:21 -0400

    init commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string input)
+        {
+            input = input.Replace(" ", "").ToLower();
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,18 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
-
+        [Theory]
+        [InlineData("racecar", true)] 
+        public void IsAPalindromeTest(string input, bool expected)
+        {          
+            WordSmith wordSmith = new WordSmith();
+            bool result = wordSmith.IsAPalindrome(input);
+            Assert.Equal(expected, result);
         }
     }
 }
